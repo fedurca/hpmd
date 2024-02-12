b98b40ac7070676de06a203e411a5c6d7186e759
Ludek Fedurca <fedurca@gmail.com>
2024-02-12 14:57:58 +0100

    flask app

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 hra pro mocninu dvou - spravedlivé slosování do pavouka
-
-docs:
-https://kivy.org/doc/stable/gettingstarted/installation.html
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def sort_names():
+    names = ""
     sorted_names = ""
     if request.method == 'POST':
         names = request.form.get('names')
         names_list = names.split('\n')
         random.shuffle(names_list)
-        sorted_names = '\n'.join(names_list)
-    return render_template('index.html', sorted_names=sorted_names)
+        sorted_names = '\n\n'.join(['\n'.join(names_list[i:i+2]) for i in range(0, len(names_list), 2)])
+    return render_template('index.html', names=names, sorted_names=sorted_names)
 
 if __name__ == '__main__':
     app.run(debug=True)
